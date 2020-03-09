8abcc77f4213f357120159cf613c7d46d3f05d4c
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-03-09 18:38:47 +0530

    DNM: raphael: drop FOD
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- device.mk
+++ device.mk
@@ -168,11 +168,11 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2-service.clearkey
 
 # Fingerprint
-PRODUCT_PACKAGES += \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_raphael
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_raphael
 
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+#PRODUCT_COPY_FILES += \
+#    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # FM
 PRODUCT_PACKAGES += \
